b6506bfc5b6190265576c3b21d59b07f3b7b920c
razanamasy <hina.razanamasy@gmail.com>
2022-08-10 15:44:08 +0200

    creation des env dans le .env et docker-compose fichier a completer a faire : installation du client mariadb dans container wordpress, creaton de start.sh avec les commandes wp-cli a completer

--- WP_docker/tools/start.sh
+++ WP_docker/tools/start.sh
@@ -1,3 +1,6 @@
-yes | wp core config --dbhost=host.db --dbname=prefix_db --dbuser=username --dbpass=password
+#!/bin/bash
 
+yes | wp core download
+yes | wp core config --dbhost=host.db --dbname=prefix_db --dbuser=username --dbpass=password
+chmod 644 wp-config.php
 wp core install --url=hrazanam.42.fr --title="coucou" --admin_name=wordpress_admin --admin_password=4Long&Strong1 --admin_email=hina.razanamasy@gmail.com
