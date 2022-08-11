9d5fe687dea3f02012b612b3146eb821d6b9c710
razanamasy <hina.razanamasy@gmail.com>
2022-08-11 12:32:31 +0200

    docker compose de tout les service commence, galere pour creation du client mariadb sur le container WP-phpfpm

--- WP_docker/start.sh
+++ WP_docker/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+
 yes | wp core download
-yes | wp core config --dbhost=host.db --dbname=prefix_db --dbuser=username --dbpass=password
+yes | wp core config --dbhost=${WP_DB_HOST} --dbname=${WP_DB_NAME} --dbuser=${WP_DB_USER} --dbpass=${WP_DB_PASS}
+${WP_DB_PASS} | mysql -u ${WP_DB_USER} -p
 chmod 644 wp-config.php
 wp core install --url=hrazanam.42.fr --title="coucou" --admin_name=wordpress_admin --admin_password=4Long&Strong1 --admin_email=hina.razanamasy@gmail.com
