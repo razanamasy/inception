b0f3ad1e5064305bbe938f92a1aac1d86453729c
razanamasy <hina.razanamasy@gmail.com>
2022-08-15 18:19:44 +0200

    docker Mariadb ok - docker wordpress a finir a la main avec le makefile - modifier dans start wp core download et se connecter a mariadb avec le user

--- WP_docker/start.sh
+++ WP_docker/start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-
-yes | wp core download
+service mysql start
+yes | wp core download 
 yes | wp core config --dbhost=${WP_DB_HOST} --dbname=${WP_DB_NAME} --dbuser=${WP_DB_USER} --dbpass=${WP_DB_PASS}
 ${WP_DB_PASS} | mysql -u ${WP_DB_USER} -p
+
 chmod 644 wp-config.php
 wp core install --url=hrazanam.42.fr --title="coucou" --admin_name=wordpress_admin --admin_password=4Long&Strong1 --admin_email=hina.razanamasy@gmail.com
-service php7-fpm restart
+service php7-fpm start
