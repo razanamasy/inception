d1744ac3455b66d1f534ae8444d9fbae4c07c588
razanamasy <hina.razanamasy@gmail.com>
2022-08-15 09:26:02 +0200

    rectif compose, ajout création compte dans maria, à faire : fichier conf wp et fpm dans docker wordpress

--- MARIA_docker/start1.sh
+++ MARIA_docker/start1.sh
@@ -1,14 +1,14 @@
-CREATE DATABASE ${MYSQL_DB_NAME}
-CREATE USER ${MYSQL_DB_USER}@localhost IDENTIFIED BY ${MYSQL_DB_PASS};
+CREATE DATABASE 'Maria' 
+CREATE USER 'hina'@localhost IDENTIFIED BY 'mdp';
 
 #only on localhost permission to acces and ue the MySQL server
-GRANT USAGE ON *.* TO ${MYSQL_DB_USER}@localhost IDENTIFIED BY ${MYSQL_DB_PASS};
+GRANT USAGE ON *.* TO 'hina'@localhost IDENTIFIED BY 'mdp';
 #Any computer on the network permission to acces and ue the MySQL server
 #GRANT USAGE ON *.* TO 'myuser'@'%' IDENTIFIED BY 'mypassword';
 
 #Grant all privileges to a user on a specific database
 #MySQL 5.7 and earlier versions
-GRANT ALL PRIVILEGES ON ${MYSQL_DB_NAME}.* TO ${MYSQL_DB_USER}@localhost IDENTIFIED BY ${MYSQL_DB_PASS};
+GRANT ALL PRIVILEGES ON 'Maria'.* TO 'hina'@localhost IDENTIFIED BY 'mdp';
 #GRANT ALL ON *.* TO ${MYSQL_DB_USER}@'localhost' IDENTIFIED BY ${MYSQL_DB_PASS} WITH GRANT OPTION;
 
 #Apply privileges
