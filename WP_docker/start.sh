98239696903199823d75fdd7ddd2f7ed4359040b
razanamasy <hina.razanamasy@gmail.com>
2022-08-16 14:55:57 +0200

    mkdir avant wp core

--- WP_docker/start.sh
+++ WP_docker/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-service mysql start
-yes | wp core download --allow-root --path="/var/www/html" 
-yes | wp core config --dbhost=${WP_DB_HOST} --dbname=${WP_DB_NAME} --dbuser=${WP_DB_USER} --dbpass=${WP_DB_PASS}
+cd /var/www/html
+yes | wp core download --allow-root --path="." 
+yes | wp core config --allow-root --dbhost=${WP_DB_HOST} --dbname=${WP_DB_NAME} --dbuser=${WP_DB_USER} --dbpass=${WP_DB_PASS}
 ${WP_DB_PASS} | mysql -u ${WP_DB_USER} -p
-wp core install --url=hrazanam.42.fr --title="coucou" --admin_name=wordpress_admin --admin_password=4Long&Strong1 --admin_email=hina.razanamasy@gmail.com
-wp user create _user _user@example.com --role=author
-service php7-fpm start
+wp core install --allow-root --url=hrazanam.42.fr --title="coucou" --admin_name=wordpress_admin --admin_password=4Long&Strong1 --admin_email=hina.razanamasy@gmail.com
+wp user create _user _user@example.com --allow-root --role=author
+service php-fpm start
