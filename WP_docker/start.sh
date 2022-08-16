04b58a2aec59de88a511e9932282405bde992aa6
razanamasy <hina.razanamasy@gmail.com>
2022-08-16 10:01:16 +0200

    ajout de l'emplacement des fichier pour nginx et index, création d'un user et allow root dans wordpress

--- WP_docker/start.sh
+++ WP_docker/start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 service mysql start
-yes | wp core download 
+yes | wp core download --allow-root --path="/var/www/html" 
 yes | wp core config --dbhost=${WP_DB_HOST} --dbname=${WP_DB_NAME} --dbuser=${WP_DB_USER} --dbpass=${WP_DB_PASS}
 ${WP_DB_PASS} | mysql -u ${WP_DB_USER} -p
-
-chmod 644 wp-config.php
 wp core install --url=hrazanam.42.fr --title="coucou" --admin_name=wordpress_admin --admin_password=4Long&Strong1 --admin_email=hina.razanamasy@gmail.com
+wp user create _user _user@example.com --role=author
 service php7-fpm start
