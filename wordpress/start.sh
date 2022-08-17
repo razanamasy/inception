598d1efbbba140a5e8240512303e7c2569da64c2
razanamasy <hina.razanamasy@gmail.com>
2022-08-17 11:56:53 +0200

    docker compose up avec les bon dockerfile mais maria et wordpress pb de config

--- wordpress/start.sh
+++ wordpress/start.sh
@@ -3,7 +3,7 @@
 cd /var/www/html
 yes | wp core download --allow-root --path="." 
 yes | wp core config --allow-root --dbhost=${WP_DB_HOST} --dbname=${WP_DB_NAME} --dbuser=${WP_DB_USER} --dbpass=${WP_DB_PASS}
-${WP_DB_PASS} | mysql -u ${WP_DB_USER} -p
+mysql -u ${WP_DB_USER} -p${WP_DB_PASS}
 wp core install --allow-root --url=hrazanam.42.fr --title="coucou" --admin_name=wordpress_admin --admin_password=4Long&Strong1 --admin_email=hina.razanamasy@gmail.com
 wp user create _user _user@example.com --allow-root --role=author
 service php7.4-fpm start
