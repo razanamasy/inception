aab7e436d320f0641f13ac614b9b3a9440fa2b37
razanamasy <hina.razanamasy@gmail.com>
2022-08-18 19:05:57 +0200

    try to resolve exit status error 1 Maria db : hint - make the volume and do something with the fileconfig ?

--- mariadb/start1.sh
+++ mariadb/start1.sh
@@ -1,12 +1,9 @@
 service mysql start
-echo "$MYSQL_DB_NAME $MYSQL_DB_USER et $MYSQL_DB_PASS"
-mysql -u root -proot  << EOF
+mariadb  << EOF
 CREATE DATABASE IF NOT EXISTS hina_db ;
 SHOW DATABASES ;
-USE hina_db ;
 CREATE USER "hina"@"wordpress.inception_my_network" IDENTIFIED BY "root";
-SELECT user FROM user ;
 GRANT ALL PRIVILEGES ON hina_db.* TO "hina"@"wordpress.inception_my_network" IDENTIFIED BY "root";
 FLUSH PRIVILEGES;
-
 EOF
+exec mysqld
--- wordpress/start.sh
+++ wordpress/start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+#sleep
+
 cd /var/www/html
 yes | wp core download --allow-root --path="." 
-yes | wp core config --allow-root --dbhost=${DB_HOST} --dbname=${WP_DB_NAME} --dbuser=${WP_DB_USER} --dbpass=${WP_DB_PASS}
-mysql -u ${WP_DB_USER} -p${WP_DB_PASS}
+yes | wp core config --allow-root --dbhost=$DB_HOST --dbname=hina_db --dbuser=hina --dbpass=root
+mysql -uhina -h$DB_HOST -proot -Dhina_db
 wp core install --allow-root --url=hrazanam.42.fr --title="coucou" --admin_name=wordpress_admin --admin_password=4Long&Strong1 --admin_email=hina.razanamasy@gmail.com
 wp user create _user _user@example.com --allow-root --role=author
 service php7.3-fpm start
