0ea65396d49eeaa00667140274bdcb26229634dc
razanamasy <hina.razanamasy@gmail.com>
2022-08-19 16:44:28 +0200

    toujours des soucis de connection a mariadb quand on ouvre au network : mariadb crash wtf

--- wordpress/start.sh
+++ wordpress/start.sh
@@ -4,8 +4,8 @@ sleep 10
 
 cd /var/www/html
 yes | wp core download --allow-root --path="." 
-yes | wp core config --allow-root --dbhost=$DB_HOST --dbname=hina_db --dbuser=hina --dbpass=root
-mysql -uhina -h$DB_HOST -proot -Dhina_db
-wp core install --allow-root --url=hrazanam.42.fr --title="coucou" --admin_name=wordpress_admin --admin_password=4Long&Strong1 --admin_email=hina.razanamasy@gmail.com
+#mysql -uhina -hmariadb.inception_my_network -proot -Dhina_db
+yes | wp config create --allow-root --dbhost=mariadb.inception_my_network --dbname=hina_db --dbuser=hina --dbpass=root --path="."
+wp core install --allow-root --url=hrazanam.42.fr --title="coucou" --admin_name=wordpress_admin --admin_password=admin_password --admin_email=hina.razanamasy@gmail.com
 wp user create _user _user@example.com --allow-root --role=author
 service php7.3-fpm start
